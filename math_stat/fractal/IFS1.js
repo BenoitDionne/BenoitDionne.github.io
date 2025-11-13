18e74d4d39304c79b338739f2071928c9c2c7000
“Benoit <bdionne@uottawa.ca>
2025-11-12 21:27:32 -0500

    More modifications to the scripts that generate the Sierpinsky triangle, fern and leaf

--- math_stat/fractal/IFS1.js
+++ math_stat/fractal/IFS1.js
@@ -1,30 +1,41 @@
 window.onload = apps_init;
 
 function apps_init() {
-    var initSRPK = document.getElementById("initSRPK");
+    if ( document.getElementById("SRPK") != null ) {
+	var initSRPK = document.getElementById("initSRPK");
+	var filledSRPK = document.getElementById("filledSRPK");
+	var colourSRPK = document.getElementById("colourSRPK");
+	var fillSrpk = false;
+	var colSrpk = "black";
+    
+	if ( filledSRPK != null )
+	    fillSrpk = ( filledSRPK.value === 'true' );
 
-    if ( initSRPK != null )
-	initSRPK.onchange = function() {
-	    var n = initSRPK.value;
-	    initObject("SRPK", true, n);
-	}
+	if ( colourSRPK != null )
+	    colSrpk = colourSRPK.value;
 
-    initObject("SRPK", true, 0);
-    initSRPK.value = 0;
-}
+	if ( initSRPK != null )
+	    initSRPK.onchange = function() {
+		initObject("SRPK", fillSrpk, colSrpk, initSRPK.value);
+	    }
 
-class Curves {
-    constructor(X,Y,S) {
-	this.curvesX = X;
-	this.curvesY = Y;
-	this.step = S;
+	initObject("SRPK", fillSrpk, colSrpk, 0);
+	initSRPK.value = 0;
     }
 }
 
-function plotCurve(cntxt,X,Y,step,fillCurve) {
+class Curves {
+    constructor(X,Y,N) {
+	this.curvesX = X;
+	this.curvesY = Y;
+	this.initNbrVtx = N;
+    }
+}
+
+function plotCurve(cntxt,X,Y,J,fillCurve) {
     cntxt.beginPath();
     cntxt.moveTo(X[0],Y[0]);
-    for ( var j = 1 ; j<step ; ++j ) {
+    for ( var j = 1 ; j<J ; ++j ) {
 	cntxt.lineTo(X[j],Y[j]);
     }
     cntxt.stroke();
@@ -43,16 +54,16 @@ function clearIFS(CnvsName) {
     }
 }
 
-function initObject(cnvsName, fillCurve, choice) {
+function initObject(cnvsName, fillCurve, colour, choice) {
     var cnvs = document.getElementById(cnvsName);
     var width = cnvs.width;
     var height = cnvs.height;
     var cntxt = cnvs.getContext("2d");
 
-    return initObject2(cntxt, width, height, choice, fillCurve);
+    return initObject2(cntxt, width, height, choice, fillCurve, colour);
 }
 
-function initObject2(cntxt, width, height, choice, fillCurve) {
+function initObject2(cntxt, width, height, choice, fillCurve, colour) {
     cntxt.clearRect(0, 0, width, height);
 
     var w2 = Math.floor(width);
@@ -61,68 +72,33 @@ function initObject2(cntxt, width, height, choice, fillCurve) {
     /* The default curve is the triangle */
     var curvesX = [0, w2, 0, 0];
     var curvesY = [0, h2, h2, 0];
-    var step = 4;
+    var initNbrVtx = 3;
 
     if ( choice == 1 ) {
 	curvesX = [0, w2, w2, 0, 0];
 	curvesY = [0, 0, h2, h2, 0];
-	step = 5;
+	initNbrVtx = 4;
     } else if ( choice == 2 ) {
 	var w3 = Math.floor(width/2);
 	var h3 = Math.floor(height/2);
 	curvesX = [w3, w2, w3, 0, w3];
 	curvesY = [0, h3, h2, h3, 0];
-	step = 5;
+	initNbrVtx = 4;
     }
 
     // The initial figure
-    cntxt.strokeStyle = 'blue';
+    cntxt.strokeStyle = colour;
     if ( fillCurve ) {
-	cntxt.fillStyle = "blue";
+	cntxt.fillStyle = colour;
     }
-    plotCurve(cntxt, curvesX, curvesY, step, fillCurve);
-    
-    return new Curves(curvesX, curvesY, step);
+    plotCurve(cntxt, curvesX, curvesY, initNbrVtx+1, fillCurve);
+
+    return new Curves(curvesX, curvesY, initNbrVtx);
 }
 
-/* Iteration on the image */
-function iterateSRPK2() {
-    var cnvs = document.getElementById("SRPK");
-    var width = cnvs.width;
-    var height = cnvs.height;
-    var cntxt = cnvs.getContext("2d");
-
-    var nbr = Number( document.getElementById("nbrSRPK").value );
-    var choice = Number( document.getElementById("initSRPK").value );
-
-    cntxt.clearRect(0, 0, width, height);
-
-    var curve = initObject2(cntxt, width, height, choice, true);
-
-    var nw = Math.floor(width/2);
-    var nh = Math.floor(height/2);
-    for ( var i = 0 ; i < nbr ; i++ ) {
-	var cnvs2 = document.createElement("canvas");
-	cnvs2.width = width;
-	cnvs2.height = height;
-	var cntxt2 = cnvs2.getContext("2d");
-	/* option 1 , both work fine
-	cntxt2.scale(0.5,0.5);
-	*/
-	cntxt2.drawImage(cnvs, 0, 0, width, height);
-	cntxt.clearRect(0, 0, cnvs.width, cnvs.height);
-	/* option 1
-	cntxt.drawImage(cnvs2, 0, 0);
-	cntxt.drawImage(cnvs2, 0, nh);
-	cntxt.drawImage(cnvs2, nw, nh);
-	*/
-	cntxt.drawImage(cnvs2, 0, 0, width, height, 0, 0, nw, nh);
-	cntxt.drawImage(cnvs2, 0, 0, width, height, 0, nh, nw, nh);
-	cntxt.drawImage(cnvs2, 0, 0, width, height, nw, nh, nw, nh);
-    }
-}
-
-/* Iteration on the pixel coordinates */
+/* Iteration on the pixel coordinates.  This function uses more computer
+   memory than the function iterateSRPK2() below and so the number of
+   iterations is limited. */ 
 function iterateSRPK() {
     var cnvs = document.getElementById("SRPK");
     var width = cnvs.width;
@@ -131,30 +107,30 @@ function iterateSRPK() {
 
     var nbr = Number( document.getElementById("nbrSRPK").value );
     var choice = Number( document.getElementById("initSRPK").value );
+    var fillSrpk = ( document.getElementById("filledSRPK").value === 'true' );
+    var colSrpk = document.getElementById("colourSRPK").value;
 
-    cntxt.clearRect(0, 0, width, height);
+    var curves = initObject2(cntxt, width, height, choice, fillSrpk, colSrpk);
 
-    var curves = initObject2(cntxt, width, height, choice, false);
-
-    for ( var i = 0 ; i < nbr ; ++i ) {
-	cntxt.clearRect(0,0, width, height);
+    for ( var i = 0 ; i < nbr ; ++i )
 	curves = nextStepSRPK(curves, width, height);
-	var curvesX = curves.curvesX;
-	var curvesY = curves.curvesY;
-	var step = curves.step;
 
-	var nb = curvesX.length;
-	for ( var j = 0 ; j < nb ; j=j+step ) {
-	    plotCurve(cntxt, curvesX.slice(j,j+step),
-		      curvesY.slice(j,j+step), step, false);
+    cntxt.clearRect(0, 0, cnvs.width, cnvs.height);
+    var curvesX = curves.curvesX;
+    var curvesY = curves.curvesY;
+    var initNbrVtx = curves.initNbrVtx;
+
+    var nb = curvesX.length;
+
+    for ( var j = 0 ; j < nb ; j= (j+initNbrVtx+1) ) {
+	plotCurve(cntxt, curvesX.slice(j,j+initNbrVtx+1), curvesY.slice(j,j+initNbrVtx+1), initNbrVtx+1, fillSrpk);
 	}
-    }
 }
 
 function nextStepSRPK(curves, w, h) {
     var curvesX = curves.curvesX;
     var curvesY = curves.curvesY;
-    var step = curves.step;
+    var initNbrVtx = curves.initNbrVtx;
 
     var lh = curvesX.length;
     var lh2 = 2*lh;
@@ -169,6 +145,48 @@ function nextStepSRPK(curves, w, h) {
 	newCurvesX[i+lh2] = Math.floor((w+curvesX[i])/2);
 	newCurvesY[i+lh2] = Math.floor((h+curvesY[i])/2);
     }
-    return new Curves(newCurvesX, newCurvesY, step);
+    return new Curves(newCurvesX, newCurvesY, initNbrVtx);
+}
+
+/* Iteration of the image.  This function can be used instead of
+   iterateSRPK().  However, if the number of iteration is large, then
+   the individual blocks of the Sierpinski become so small that they
+   are basically represented by one pixel.  So, the drawing becomes
+   basically invisible. */
+function iterateSRPK2() {
+    var cnvs = document.getElementById("SRPK");
+    var width = cnvs.width;
+    var height = cnvs.height;
+    var cntxt = cnvs.getContext("2d");
+
+    var nbr = Number( document.getElementById("nbrSRPK").value );
+    var choice = Number( document.getElementById("initSRPK").value );
+    var fillSrpk = ( document.getElementById("filledSRPK").value === 'true' );
+    var colSrpk = document.getElementById("colourSRPK").value;
+    
+    initObject2(cntxt, width, height, choice, fillSrpk, colSrpk);
+
+    var nw = Math.floor(width/2);
+    var nh = Math.floor(height/2);
+    for ( var i = 0 ; i < nbr ; i++ ) {
+	var cnvs2 = document.createElement("canvas");
+	cnvs2.width = width;
+	cnvs2.height = height;
+	var cntxt2 = cnvs2.getContext("2d");
+
+	cntxt2.drawImage(cnvs, 0, 0, width, height);
+	cntxt.clearRect(0, 0, cnvs.width, cnvs.height);
+	cntxt.drawImage(cnvs2, 0, 0, width, height, 0, 0, nw, nh);
+	cntxt.drawImage(cnvs2, 0, 0, width, height, 0, nh, nw, nh);
+	cntxt.drawImage(cnvs2, 0, 0, width, height, nw, nh, nw, nh);
+	/* Second option, both work fine
+	   cntxt2.drawImage(cnvs, 0, 0, width, height);
+	   cntxt2.scale(0.5,0.5);
+	   cntxt.clearRect(0, 0, cnvs.width, cnvs.height);
+	   cntxt.drawImage(cnvs2, 0, 0);
+	   cntxt.drawImage(cnvs2, 0, nh);
+	   cntxt.drawImage(cnvs2, nw, nh);
+	*/
+    }
 }
 
--- math_stat/fractal/IFS2.js
+++ math_stat/fractal/IFS2.js
@@ -1,31 +1,49 @@
 window.onload = apps_init;
 
 function apps_init() {
-    var initFERN = document.getElementById("initFERN");
-    var fillFern = ( document.getElementById("filledFERN").value === 'true' );
-    var colFern = document.getElementById("colourFERN").value;
+    if ( document.getElementById("FERN") != null ) {
+	var initFERN = document.getElementById("initFERN");
+	var filledFERN = document.getElementById("filledFERN");
+	var colourFERN = document.getElementById("colourFERN");
+	var fillFern = false;
+	var colFern = "black";
+    
+	if ( filledFERN != null )
+	    fillFern = ( filledFERN.value === 'true' );
 
-    if ( initFERN != null )
-	initFERN.onchange = function() {
-	    var n = initFERN.value;
-	    initObject("FERN", fillFern, colFern, n);
-	}
+	if ( colourFERN != null )
+	    colFern = colourFERN.value;
 
-    initObject("FERN", fillFern, colFern, 0);
-    initFERN.value = 0;
+	if ( initFERN != null )
+	    initFERN.onchange = function() {
+		initObject("FERN", fillFern, colFern, initFERN.value);
+	    }
 
-    var initLEAF = document.getElementById("initLEAF");
-    var fillLeaf = ( document.getElementById("filledLEAF").value === 'true' );
-    var colLeaf = document.getElementById("colourLEAF").value;
+	initObject("FERN", fillFern, colFern, 0);
+	initFERN.value = 0;
+    }
 
-    if ( initLEAF != null )
-	initLEAF.onchange = function() {
-	    var n = initLEAF.value;
-	    initObject("LEAF", fillLeaf, colLeaf, n);
-	}
+    if ( document.getElementById("LEAF") != null ) {
+	var initLEAF = document.getElementById("initLEAF");
+	var filledLEAF = document.getElementById("filledLEAF");
+	var colourLEAF = document.getElementById("colourLEAF");
+	var fillLeaf = false;
+	var colLeaf = "black";
+    
+	if ( filledLEAF != null )
+	    fillLeaf = ( filledLEAF.value === 'true' );
 
-    initObject("LEAF", fillLeaf, colLeaf, 0);
-    initLEAF.value = 0;
+	if ( colourLEAF != null )
+	    colLeaf = colourLEAF.value;
+
+	if ( initLEAF != null )
+	    initLEAF.onchange = function() {
+		initObject("LEAF", fillLeaf, colLeaf, initLEAF.value);
+	    }
+
+	initObject("LEAF", fillLeaf, colLeaf, 0);
+	initLEAF.value = 0;
+    }
 }
 
 /* *********************************************************************** */
@@ -39,17 +57,22 @@ function clearIFS(CnvsName) {
 }
 
 class Curves {
-    constructor(X,Y,S) {
+    constructor(X,Y,N) {
 	this.curvesX = X;
 	this.curvesY = Y;
-	this.step = S;
+	this.initNbrVtx = N;   /* The number of vertices of the */
+	                       /* initial figure */ 
     }
 }
 
-function plotCurve(cntxt,X,Y,step,fillCurve) {
+function plotCurve(cntxt,curve,fillCurve) {
+    var X = curve.curvesX;
+    var Y = curve.curvesY;
+    var J = curve.initNbrVtx;
+ 
     cntxt.beginPath();
     cntxt.moveTo(X[0],Y[0]);
-    for ( var j = 1 ; j<step ; ++j ) {
+    for ( var j = 1 ; j<J ; ++j ) {
 	cntxt.lineTo(X[j],Y[j]);
     }
     cntxt.stroke();
@@ -74,28 +97,29 @@ function initObject(cnvsName, fillCurve, colour, choice) {
     /* The default curve is the triangle */
     var curvesX = [0, w2, 0, 0];
     var curvesY = [0, h2, h2, 0];
-    var step = 4;
-
+    var initNbrVtx = 3;
+    
     if ( choice == 1 ) {
 	curvesX = [0, w2, w2, 0, 0];
 	curvesY = [0, 0, h2, h2, 0];
-	step = 5;
+	initNbrVtx = 4;
     } else if ( choice == 2 ) {
 	var w3 = Math.floor(width/2);
 	var h3 = Math.floor(height/2);
 	curvesX = [w3, w2, w3, 0, w3];
 	curvesY = [0, h3, h2, h3, 0];
-	step = 5;
+	initNbrVtx = 4;
     }
-
+    
     // The initial figure
     cntxt.strokeStyle = colour;
     if ( fillCurve ) {
 	cntxt.fillStyle = colour;
     }
-    plotCurve(cntxt, curvesX, curvesY, step, fillCurve);
     
-    return new Curves(curvesX, curvesY, step);
+    var newcurve = new Curves(curvesX, curvesY, initNbrVtx);
+    plotCurve(cntxt, newcurve, fillCurve);
+    return newcurve;
 }
 
 /* *********************************************************************** */
@@ -103,24 +127,24 @@ function initObject(cnvsName, fillCurve, colour, choice) {
 /* These class contains the real coordinates, not the pixel coordinates,
    of the curves  */
 class Curves2 {
-    constructor(X,Y,S) {
+    constructor(X,Y,N) {
 	this.curvesX = X;
 	this.curvesY = Y;
-	this.step = S;
+	this.initNbrVtx = N;
 	this.mx = 0;
 	this.Mx = 0;
 	this.my = 0;
 	this.My = 0;
     }
     minMAX() {
-	var N = this.curvesX.length;
+	var J = this.curvesX.length;
 	this.mx = this.Mx = this.curvesX[0];
 	this.my = this.My = this.curvesY[0];
-	for (var i=1 ; i < N ; ++i ){
-	    this.mx = Math.min(this.mx, this.curvesX[i]);
-	    this.Mx = Math.max(this.Mx, this.curvesX[i]);
-	    this.my = Math.min(this.my, this.curvesY[i]);
-	    this.My = Math.max(this.My, this.curvesY[i]);
+	for (var j=1 ; j < J ; ++j ){
+	    this.mx = Math.min(this.mx, this.curvesX[j]);
+	    this.Mx = Math.max(this.Mx, this.curvesX[j]);
+	    this.my = Math.min(this.my, this.curvesY[j]);
+	    this.My = Math.max(this.My, this.curvesY[j]);
 	}
     }	
 }
@@ -135,18 +159,18 @@ function initObject4(cnvs, fillCurve, colour, choice) {
     /* The default curve is the triangle */
     var curvesX = [0, 1, 0, 0];
     var curvesY = [0, 0, 1, 0];
-    var step = 4;
-
+    var initNbrVtx = 3;
+    
     if ( choice == 1 ) {
 	curvesX = [0, 1, 1, 0, 0];
 	curvesY = [0, 0, 1, 1, 0];
-	step = 5;
+	initNbrVtx = 4;
     } else if ( choice == 2 ) {
 	curvesX = [1/2, 1, 1/2, 0, 1/2];
 	curvesY = [0, 1/2, 1, 1/2, 0];
-	step = 5;
+	initNbrVtx = 4;
     }
-    var newcurves = new Curves2(curvesX, curvesY, step);
+    var newcurves = new Curves2(curvesX, curvesY, initNbrVtx);
 
     // The initial figure
     cntxt.strokeStyle = colour;
@@ -154,7 +178,7 @@ function initObject4(cnvs, fillCurve, colour, choice) {
 	cntxt.fillStyle = colour;
     }
     plotCurve2(cnvs, newcurves, fillCurve);
-    
+
     return newcurves;
 }
 
@@ -165,7 +189,7 @@ function plotCurve2(cnvs, curves, fillCurve) {
     var height = cnvs.height;
     var cntxt = cnvs.getContext("2d");
 
-    var step = curves.step;
+    var J = curves.initNbrVtx;
     var N = curves.curvesX.length;
     var X = curves.curvesX;
     var Y = curves.curvesY;
@@ -191,11 +215,11 @@ function plotCurve2(cnvs, curves, fillCurve) {
     /* to center vertically the figure */
     var h0 = height - Math.max(0, Math.floor((height - (My - my)*delta)/2));
 
-    for ( var j = 0 ; j < N ; j=j+step ) {
+    for ( var j = 0 ; j < N ; j=j+J ) {
 	cntxt.beginPath();
 	cntxt.moveTo(w0 + Math.floor((X[j+0]-mx)*delta),
 		     h0 - Math.floor((Y[j+0]-my)*delta));
-	for ( var i = 1 ; i<step ; ++i ) {
+	for ( var i = 1 ; i<J ; ++i ) {
 	    cntxt.lineTo(w0 + Math.floor((X[j+i]-mx)*delta),
 			 h0 - Math.floor((Y[j+i]-my)*delta));
 	}
@@ -207,6 +231,8 @@ function plotCurve2(cnvs, curves, fillCurve) {
 	cntxt.save();
     }
 }
+
+/* *********************************************************************** */
 
 /* Iterations on the real coordinates */
 function iterateFERN() {
@@ -220,21 +246,19 @@ function iterateFERN() {
     var fillFern = ( document.getElementById("filledFERN").value === 'true' );
     var colFern = document.getElementById("colourFERN").value;
 
-    cntxt.clearRect(0, 0, width, height);
-
     var curves = initObject4(cnvs, fillFern, colFern, choice);
 
-    for ( var i = 0 ; i < nbr ; ++i ) {
-	cntxt.clearRect(0,0, width, height);
+    for ( var i = 0 ; i < nbr ; ++i )
 	curves = nextStepFERN(curves);
-	plotCurve2(cnvs, curves, fillFern);
-    }
+    
+    cntxt.clearRect(0,0, width, height);
+    plotCurve2(cnvs, curves, fillFern);
 }
 
 function nextStepFERN(curves) {
     var curvesX = curves.curvesX;
     var curvesY = curves.curvesY;
-    var step = curves.step;
+    var initNbrVtx = curves.initNbrVtx;
 
     var lh = curvesX.length;
     var lh2 = 2*lh;
@@ -263,8 +287,10 @@ function nextStepFERN(curves) {
 	newCurvesX[i+lh3] = -Fg*curvesX[i] + Fh*curvesY[i];
 	newCurvesY[i+lh3] = Fi*curvesX[i] + Fj*curvesY[i] + 0.44;
     }
-    return new Curves2(newCurvesX, newCurvesY, step);
+    return new Curves2(newCurvesX, newCurvesY, initNbrVtx);
 }
+
+/* *********************************************************************** */
 
 /* Iterations on the real coordinates */
 function iterateLEAF() {
@@ -278,21 +304,19 @@ function iterateLEAF() {
     var fillLeaf = ( document.getElementById("filledLEAF").value === 'true' );
     var colLeaf = document.getElementById("colourLEAF").value;
 
-    cntxt.clearRect(0, 0, width, height);
-
     var curves = initObject4(cnvs, fillLeaf, colLeaf, choice);
 
-    for ( var i = 0 ; i < nbr ; ++i ) {
-	cntxt.clearRect(0,0, width, height);
+    for ( var i = 0 ; i < nbr ; ++i )
 	curves = nextStepLEAF(curves);
-	plotCurve2(cnvs, curves, fillLeaf);
-    }
+
+    cntxt.clearRect(0,0, width, height);
+    plotCurve2(cnvs, curves, fillLeaf);
 }
 
 function nextStepLEAF(curves) {
     var curvesX = curves.curvesX;
     var curvesY = curves.curvesY;
-    var step = curves.step;
+    var initNbrVtx = curves.initNbrVtx;
 
     var lh = curvesX.length;
     var lh2 = 2*lh;
@@ -310,246 +334,5 @@ function nextStepLEAF(curves) {
 	newCurvesX[i+lh3] = 0.4*curvesX[i] - 0.3*curvesY[i] + 0.27;
 	newCurvesY[i+lh3] = 0.3*curvesX[i] + 0.4*curvesY[i] + 0.09;
     }
-    return new Curves2(newCurvesX, newCurvesY, step);
+    return new Curves2(newCurvesX, newCurvesY, initNbrVtx);
 }
-
-/* *********************************************************************** */
-
-/* Not used */
-/*
-function initObject3(cnvsName, fillCurve, choice) {
-    var cnvs = document.getElementById(cnvsName);
-
-    return initObject4(cnvs, fillCurve, choice);
-}
-*/
-
-/* Work in progress */
-function initObject5(cnvs, fillCurve, choice) {
-    var width = cnvs.width;
-    var height = cnvs.height;
-    var cntxt = cnvs.getContext("2d");
-
-    var dim = Math.min(height,width);
-    var mid = Math.floor(width/2);
-    var a = Math.floor(mid/2);
-    var b = Math.floor(mid/4);
-
-    cntxt.clearRect(0, 0, width, height);
-
-    /* The default curve is the triangle */
-    var X = [mid, mid + a, mid, mid];
-    var Y = [dim, dim, dim - a, dim];
-    var step = 4;
-
-    if ( choice == 1 ) {
-	X = [mid, mid+a, mid+a, mid, mid];
-	Y = [dim, dim, dim-a, dim-a, dim];
-	step = 5;
-    } else if ( choice == 2 ) {
-	X = [mid, mid+b, mid+a, mid+b, mid];
-	Y = [dim-b, dim, dim-b, dim-a, dim-b];
-	step = 5;
-    }
-
-    cntxt.beginPath();
-    cntxt.moveTo(X[0],Y[0]);
-    for ( var j = 1 ; j<step ; ++j ) {
-	cntxt.lineTo(X[j],Y[j]);
-    }
-    cntxt.stroke();
-    cntxt.closePath();
-    if ( fillCurve ) {
-    	cntxt.fill();
-    }
-    cntxt.save();
-}
-
-/* Iterate on the real coordinates but store the information in pixels. */
-function iterateFERN2() {
-    var cnvs = document.getElementById("FERN");
-    var width = cnvs.width;
-    var height = cnvs.height;
-    var cntxt = cnvs.getContext("2d");
-
-    var nbr = Number( document.getElementById("nbrFERN").value );
-    var choice = Number( document.getElementById("initFERN").value );
-
-    cntxt.clearRect(0, 0, width, height);
-
-    var curves = initObject5(cnvs, false, choice);
-
-    var dim = Math.min(height,width);
-    var mid = Math.floor(width/2);
-    var cx = 0;
-    var cy = 0;
-    var R = 9;
-    for ( var i = 0 ; i < nbr ; ++i ) {
-	nextStepFERN2(cnvs, cntxt, dim, mid, cx, cy, R);
-    }
-}
-
-function nextStepFERN2(cnvs, cntxt, dim, mid, cx, cy, R) {
-    var cnvs_data_init = cntxt.getImageData(0, 0, dim, dim);
-
-    cntxt.clearRect(0,0, cnvs.width, cnvs.height);
-    var cnvs_data = cntxt.getImageData(0, 0, dim, dim);
-    var coeff = new Array(16);
-    var transl = new Array(8);
-    coeff[0] = 0;
-    coeff[1] = 0;
-    coeff[2] = 0;
-    coeff[3] = 0.16;
-    transl[0] = 0;
-    transl[1] = 0;
-    
-    coeff[4] = 0.84925440558500;
-    coeff[5] = 0.03559430566200;
-    coeff[6] = -0.03559430566200;
-    coeff[7] = 0.84925440558500;
-    transl[2] = 0;
-    transl[3] = 1.6;
-
-    coeff[8] = 0.19681770870000;
-    coeff[9] = -0.25660125726800;
-    coeff[10] = 0.22641287406000;
-    coeff[11] = 0.22306006986000;
-    transl[4] = 0;
-    transl[5] = 1.6;
-
-    coeff[12] = -0.15000000015000;
-    coeff[13] = 0.22981333296000;
-    coeff[14] = 0.32042939929500;
-    coeff[15] = 0.23783141551500;    
-    transl[6] = 0;
-    transl[7] = 0.44;
-    
-    for ( var i=1 ; i<dim ; ++i ) {
-	for ( var j=1 ; j<dim ; ++ j ) {
-	    if ( isPixelSet(i, j, dim, cnvs_data_init) ) {
-		var x = R*(i - mid)/mid + cx;
-		var y = R*(mid - j)/mid + cy;
-		for ( var k = 0 ; k < 13 ; k = k+4 ) {
-		    var xr = coeff[0+k]*x + coeff[1+k]*y + transl[0+k/2];
-		    var yr = coeff[2+k]*x + coeff[3+k]*y + transl[1+k/2];
-		    drawPixel(xr, yr, cx, cy, R, 0, 0, 0, 255, mid, dim, cnvs_data);
-		}
-	    }
-	}
-    }
-    cntxt.putImageData(cnvs_data, 0, 0);
-}
-
-function drawPixel(x, y, cx, cy, R, r, g, b, a, mid, dim, cnvs_data) {
-    var i = Math.floor(mid*(R + x - cx)/R);
-    var j = Math.floor(mid*(R - y + cx)/R);
-    var index = (i + j*dim)*4;
-
-    if ( index < dim*dim*4 && index >= 0 ) {
-	cnvs_data.data[index + 0] = r;
-	cnvs_data.data[index + 1] = g;
-	cnvs_data.data[index + 2] = b;
-	cnvs_data.data[index + 3] = a;
-    }
-}
-
-function isPixelSet(i, j, dim, cnvs_data) {
-    var index = (i + j*dim)*4;
-    return cnvs_data.data[index] == 0;
-}
-
-/* ****************************************************************** */
-/*
-function initObject2(cntxt,width,height,choice,fillCurve) {
-    cntxt.clearRect(0, 0, width, height);
-
-    var w2 = Math.floor(width);
-    var h2 = Math.floor(height);
-
-    /* The default curve is the triangle */
-/*
-    var curvesX = [0, w2, 0, 0];
-    var curvesY = [0, h2, h2, 0];
-    var step = 4;
-
-    if ( choice == 1 ) {
-	curvesX = [0, w2, w2, 0, 0];
-	curvesY = [0, 0, h2, h2, 0];
-	step = 5;
-    } else if ( choice == 2 ) {
-	var w3 = Math.floor(width/2);
-	var h3 = Math.floor(height/2);
-	curvesX = [w3, w2, w3, 0, w3];
-	curvesY = [0, h3, h2, h3, 0];
-	step = 5;
-    }
-
-    // The initial figure
-    cntxt.strokeStyle = 'blue';
-    if ( fillCurve ) {
-	cntxt.fillStyle = "blue";
-    }
-    plotCurve(cntxt, curvesX, curvesY, step, fillCurve);
-    
-    return new Curves(curvesX, curvesY, step);
-}
-
-/* Work in progress / doesn't work
-   Without the existence of negative pixel coordinates, I don't see how
-   we can obtain a method significantly more efficient than the method
-   used above. */
-/*
-function iterateFERN2(fill) {
-    var cnvs = document.getElementById("FERN");
-    var width = cnvs.width;
-    var height = cnvs.height;
-    var cntxt = cnvs.getContext("2d");
-
-    var nbr = Number( document.FERN_data.nbr.value );
-    var choice = Number( document.FERN_data.choice.value );
-
-    cntxt.clearRect(0, 0, width, height);
-
-    var curve = initObject2(cntxt,Math.floor(width/3),Math.floor(height/3),
-			    choice,fill);
-
-    var Fa = 0.84925440558500;
-    var Fb = 0.03559430566200;
-    var Fc = 0.19681770870000;
-    var Fd = 0.25660125726800;
-    var Fe = 0.22641287406000;
-    var Ff = 0.22306006986000;
-    var Fg = 0.15000000015000;
-    var Fh = 0.22981333296000;
-    var Fi = 0.32042939929500;
-    var Fj = 0.23783141551500;    
-
-    for ( var i = 0 ; i < nbr ; i++ ) {
-	var cnvs2 = document.createElement("canvas");
-	cnvs2.width = width;
-	cnvs2.height = height;
-	var cntxt2 = cnvs2.getContext("2d");
-	cntxt2.drawImage(cnvs, 0, 0, width, height);
-	cntxt.clearRect(0, 0, cnvs.width, cnvs.height);
-
-	var drift = Math.floor(width/3);
-	
-	cntxt.setTransform(0.005,0,0,0.16,drift,Math.floor(0.84*height));
-	cntxt.drawImage(cnvs2, 0, 0);
-
-	cntxt.setTransform(Fa,Fb,-Fb,Fa,drift,Math.floor(0.2*height));
-	cntxt.drawImage(cnvs2, 0, 0);
-	
-	cntxt.setTransform(Fc,-Fd,Fe,Ff,0,drift,Math.floor(0.2*height));
-	cntxt.drawImage(cnvs2, 0, 0);
-
-	cntxt.setTransform(-Fg,Fh,Fi,Fj,drift,Math.floor(0.78*height));
-	cntxt.drawImage(cnvs2, 0, 0);
-	
-	cntxt.setTransform(1,0,0,1,0,0);	
-    }
-}
-*/
-
-
-
