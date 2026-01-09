ba39b59e6defcfdf7922c2243bab27ad889d8c56
“Benoit <bdionne@uottawa.ca>
2026-01-09 17:09:39 -0500

    Replace IFS2.js with a new javascript program called IFS3.js, modify some statements.

--- math_stat/fractal/IFS3.js
+++ math_stat/fractal/IFS3.js
@@ -79,22 +79,20 @@ function clearIFS(CnvsName) {
 }
 
 class Curves {
-    constructor(X,Y,N) {
-	this.curvesX = X;
-	this.curvesY = Y;
-	this.initNbrVtx = N;   /* The number of vertices of the */
-	                       /* initial figure */ 
+    constructor(X,Y) {
+	this.X = X;
+	this.Y = Y;
     }
 }
 
 function plotCurve(cntxt,curve,fillCurve) {
-    var X = curve.curvesX;
-    var Y = curve.curvesY;
-    var J = curve.initNbrVtx;
+    var X = curve.X;
+    var Y = curve.Y;
+    var J = curve.X.length;
  
     cntxt.beginPath();
     cntxt.moveTo(X[0],Y[0]);
-    for ( var j = 1 ; j<J ; ++j ) {
+    for ( var j = 1 ; j < J ; ++j ) {
 	cntxt.lineTo(X[j],Y[j]);
     }
     cntxt.stroke();
@@ -124,13 +122,11 @@ function initObject(cnvsName, fillCurve, colour, choice) {
     if ( choice == 1 ) {
 	curvesX = [0, w2, w2, 0, 0];
 	curvesY = [0, 0, h2, h2, 0];
-	initNbrVtx = 4;
     } else if ( choice == 2 ) {
 	var w3 = Math.floor(width/2);
 	var h3 = Math.floor(height/2);
 	curvesX = [w3, w2, w3, 0, w3];
 	curvesY = [0, h3, h2, h3, 0];
-	initNbrVtx = 4;
     }
     
     // The initial figure
@@ -139,87 +135,128 @@ function initObject(cnvsName, fillCurve, colour, choice) {
 	cntxt.fillStyle = colour;
     }
     
-    var newcurve = new Curves(curvesX, curvesY, initNbrVtx);
+    var newcurve = new Curves(curvesX, curvesY);
     plotCurve(cntxt, newcurve, fillCurve);
+
     return newcurve;
 }
 
 /* *********************************************************************** */
 
-/* These class contains the real coordinates, not the pixel coordinates,
-   of the curves  */
-class Curves2 {
-    constructor(X,Y,N) {
-	this.curvesX = X;
-	this.curvesY = Y;
-	this.initNbrVtx = N;
-	this.mx = 0;
-	this.Mx = 0;
-	this.my = 0;
-	this.My = 0;
+/* These class contains the equivalent of pixel coordinates.
+   Namely, the discretized coordinates of the rescaled IFS.
+   M is the rescaling factor of the IFS.  */
+class Points {
+    constructor(X,Y,M) {
+	this.X = [];
+	this.Y = [];
+	var J = X.length;
+ 	for (var j=0 ; j < J ; ++j ){
+	    this.X.push(Math.floor(M*X[j]));
+	    this.Y.push(Math.floor(M*Y[j]));
+	}
+	this.M = M;
     }
     minMAX() {
-	var J = this.curvesX.length;
-	this.mx = this.Mx = this.curvesX[0];
-	this.my = this.My = this.curvesY[0];
-	for (var j=1 ; j < J ; ++j ){
-	    this.mx = Math.min(this.mx, this.curvesX[j]);
-	    this.Mx = Math.max(this.Mx, this.curvesX[j]);
-	    this.my = Math.min(this.my, this.curvesY[j]);
-	    this.My = Math.max(this.My, this.curvesY[j]);
+	var J = this.X.length;
+	this.mx = this.Mx = this.X[0];
+	this.my = this.My = this.Y[0];
+ 	for (var j=1 ; j < J ; ++j ){
+	    this.mx = Math.min(this.mx, this.X[j]);
+	    this.Mx = Math.max(this.Mx, this.X[j]);
+	    this.my = Math.min(this.my, this.Y[j]);
+	    this.My = Math.max(this.My, this.Y[j]);
 	}
-    }	
+    }
+    create(fillFig) {
+	/* The option fillFig has not been implemented and may not
+	   be implemented in the future.  Using polygonal curves already
+	   taxes the computation time and memory space allocated to
+	   the browsers.  Adding all the points in the interior of a
+	   closed polygonal curve will probably be too much. */
+	var J = this.X.length;
+	var X1 = this.X[0];
+	var Y1 = this.Y[0];
+	for (var j = 1 ; j < J ; ++j) {
+	    var X2 = this.X[j];
+	    var Y2 = this.Y[j];
+	    var R = Math.max(Math.abs(X1-X2),Math.abs(Y1-Y2));
+	    var lx = (X2-X1)/R;
+	    var ly = (Y2-Y1)/R;
+	    for ( var k = 1 ; k < R ; ++k) {
+		this.X.push(Math.floor(X1 + k*lx));
+		this.Y.push(Math.floor(Y1 + k*ly));
+	    }
+	    X1 = X2;
+	    Y1 = Y2;
+	}
+    }
+    push(X,Y) {
+	/* Existing points are not included again. */
+	var flag = 0;
+	var idxX = this.X.indexOf(X);
+	while ( idxX != -1 ) {
+	    if ( this.Y[idxX] == Y ) {
+		flag = 1;
+		break;
+	    }
+	    idxX = this.X.indexOf(X, idxX + 1);
+	}
+	if ( flag == 0 ) {
+	    this.X.push(X);
+	    this.Y.push(Y);
+	}
+    }
 }
 
-function initObject4(cnvs, fillCurve, colour, choice) {
+function initPoints(cnvs, M, fillFig, colour, choice) {
     var width = cnvs.width;
     var height = cnvs.height;
     var cntxt = cnvs.getContext("2d");
-    
+
     cntxt.clearRect(0, 0, width, height);
 
     /* The default curve is the triangle */
-    var curvesX = [0, 1, 0, 0];
-    var curvesY = [0, 0, 1, 0];
-    var initNbrVtx = 3;
+    var X = [0, 1, 0, 0];
+    var Y = [0, 0, 1, 0];
     
     if ( choice == 1 ) {
-	curvesX = [0, 1, 1, 0, 0];
-	curvesY = [0, 0, 1, 1, 0];
-	initNbrVtx = 4;
+	X = [0, 1, 1, 0, 0];
+	Y = [0, 0, 1, 1, 0];
     } else if ( choice == 2 ) {
-	curvesX = [1/2, 1, 1/2, 0, 1/2];
-	curvesY = [0, 1/2, 1, 1/2, 0];
-	initNbrVtx = 4;
+	X = [1/2, 1, 1/2, 0, 1/2];
+	Y = [0, 1/2, 1, 1/2, 0];
     }
-    var newcurves = new Curves2(curvesX, curvesY, initNbrVtx);
 
-    // The initial figure
+    var points = new Points(X, Y, M);
+    points.create(fillFig);
+    
+    /* Both strokeStyle and fillStyle are set to the chosen colour.
+       This does not mean that the interior of closed polygonal curve
+       is coloured.  Only in the interior of each point is coloured. */  
     cntxt.strokeStyle = colour;
-    if ( fillCurve ) {
-	cntxt.fillStyle = colour;
-    }
-    plotCurve2(cnvs, newcurves, fillCurve);
+    cntxt.fillStyle = colour;
+    plotPoints(cnvs, points);
 
-    return newcurves;
+    return points;
 }
 
-/* This function plots the curves from the real coordinates, not the
-   pixel coordinates. */
-function plotCurve2(cnvs, curves, fillCurve) {
+/* This function plots the discretized coordinates of the rescaled IFS
+   into the original canvas. */
+function plotPoints(cnvs, points) {
     var width = cnvs.width;
     var height = cnvs.height;
     var cntxt = cnvs.getContext("2d");
+    
+    var X = points.X;
+    var Y = points.Y;
+    var J = X.length;
 
-    var J = curves.initNbrVtx + 1;
-    var N = curves.curvesX.length;
-    var X = curves.curvesX;
-    var Y = curves.curvesY;
-    curves.minMAX();
-    var mx = curves.mx;
-    var Mx = curves.Mx;
-    var my = curves.my;
-    var My = curves.My;
+    points.minMAX();
+    var mx = points.mx;
+    var Mx = points.Mx;
+    var my = points.my;
+    var My = points.My;
 
     var deltax, deltay;
     if ( My - my != 0 )
@@ -232,26 +269,14 @@ function plotCurve2(cnvs, curves, fillCurve) {
     else
 	deltax = 1;
 
-    for ( var j = 0 ; j < N ; j=j+J ) {
-	cntxt.beginPath();
-	cntxt.moveTo(Math.floor((X[j]-mx)*deltax),
-		     height - Math.floor((Y[j]-my)*deltay));
-	for ( var i = 1 ; i<J ; ++i ) {
-	    cntxt.lineTo(Math.floor((X[j+i]-mx)*deltax),
-			 height - Math.floor((Y[j+i]-my)*deltay));
-	}
-	cntxt.stroke();
-	cntxt.closePath();
-	if ( fillCurve ) {
-	    cntxt.fill();
-	}
-	cntxt.save();
+    for ( var j = 0 ; j < J ; ++j ) {
+	cntxt.fillRect(Math.floor((points.X[j]-mx)*deltax),height - Math.floor((points.Y[j]-my)*deltay),1,1);
     }
 }
 
 /* *********************************************************************** */
 
-/* Iterations on the real coordinates */
+/* Iterations on the discretized coordinates of the rescaled IFS */
 function iterateSRPK() {
     var cnvs = document.getElementById("SRPK");
     var width = cnvs.width;
@@ -262,40 +287,42 @@ function iterateSRPK() {
     var choice = Number( document.getElementById("initSRPK").value );
     var fillSrpk = ( document.getElementById("filledSRPK").value === 'true' );
     var colSrpk = document.getElementById("colourSRPK").value;
-
-    var curves = initObject4(cnvs, fillSrpk, colSrpk, choice);
+    var M = Number( document.getElementById("scaleSRPK").value );
+    
+    var points = initPoints(cnvs, M, fillSrpk, colSrpk, choice);
 
     for ( var i = 0 ; i < nbr ; ++i )
-	curves = nextStepSRPK(curves);
+	points = nextStepSRPK(points);
 
     cntxt.clearRect(0,0, width, height);
-    plotCurve2(cnvs, curves, fillSrpk);
+    plotPoints(cnvs, points);
 }
 
-function nextStepSRPK(curves) {
-    var curvesX = curves.curvesX;
-    var curvesY = curves.curvesY;
-    var initNbrVtx = curves.initNbrVtx;
+function nextStepSRPK(points) {
+    var X = points.X;
+    var Y = points.Y;
+    var M = points.M;
+    var J = X.length;
 
-    var lh = curvesX.length;
-    var lh2 = 2*lh;
-    var newCurvesX = new Array(3*lh);
-    var newCurvesY = new Array(3*lh);
+    var newPoints = new Points([],[],M);
 
-    for (var i=0 ; i < lh ; ++i ){
-	newCurvesX[i] = 0.5*curvesX[i];
-	newCurvesY[i] = 0.5*curvesY[i];
-	newCurvesX[i+lh] = 0.5*curvesX[i];
-	newCurvesY[i+lh] = 0.5 + 0.5*curvesY[i];
-	newCurvesX[i+lh2] = 0.5 + 0.5*curvesX[i];
-	newCurvesY[i+lh2] = 0.5*curvesY[i];
+    for (var j=0 ; j < J ; ++j ) {
+	var X1 = 0.5*X[j];
+	var Y1 = 0.5*Y[j] + M*0.5;
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.5*X[j] + M*0.5;
+	Y1 = 0.5*Y[j];
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.5*X[j];
+	Y1 = 0.5*Y[j];
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
     }
-    return new Curves2(newCurvesX, newCurvesY, initNbrVtx);
+    return newPoints;
 }
 
 /* *********************************************************************** */
 
-/* Iterations on the real coordinates */
+/* Iterations on the discretized coordinates of the rescaled IFS */
 function iterateFERN() {
     var cnvs = document.getElementById("FERN");
     var width = cnvs.width;
@@ -306,54 +333,45 @@ function iterateFERN() {
     var choice = Number( document.getElementById("initFERN").value );
     var fillFern = ( document.getElementById("filledFERN").value === 'true' );
     var colFern = document.getElementById("colourFERN").value;
+    var M = Number( document.getElementById("scaleFERN").value );
 
-    var curves = initObject4(cnvs, fillFern, colFern, choice);
+    var points = initPoints(cnvs, M, fillFern, colFern, choice);
 
     for ( var i = 0 ; i < nbr ; ++i )
-	curves = nextStepFERN(curves);
+	points = nextStepFERN(points);
     
     cntxt.clearRect(0,0, width, height);
-    plotCurve2(cnvs, curves, fillFern);
+    plotPoints(cnvs, points);
 }
 
-function nextStepFERN(curves) {
-    var curvesX = curves.curvesX;
-    var curvesY = curves.curvesY;
-    var initNbrVtx = curves.initNbrVtx;
+function nextStepFERN(points) {
+    var X = points.X;
+    var Y = points.Y;
+    var M = points.M;
+    var J = X.length;
 
-    var lh = curvesX.length;
-    var lh2 = 2*lh;
-    var lh3 = 3*lh;
-    var newCurvesX = new Array(4*lh);
-    var newCurvesY = new Array(4*lh);
+    var newPoints = new Points([],[],M);
 
-    var Fa = 0.85; /* 0.84925440558500; */
-    var Fb = 0.04; /* 0.03559430566200; */
-    var Fc = 0.20; /* 0.19681770870000; */
-    var Fd = 0.26; /* 0.25660125726800; */
-    var Fe = 0.23; /* 0.22641287406000; */
-    var Ff = 0.22; /* 0.22306006986000; */
-    var Fg = 0.15; /* 0.15000000015000; */
-    var Fh = 0.28; /* 0.22981333296000; */
-    var Fi = 0.26; /* 0.32042939929500; */
-    var Fj = 0.24; /* 0.23783141551500; */   
-
-    for (var i=0 ; i < lh ; ++i ){
-	newCurvesX[i] = 0;
-	newCurvesY[i] = 0.16*curvesY[i];
-	newCurvesX[i+lh] = Fa*curvesX[i] + Fb*curvesY[i];
-	newCurvesY[i+lh] = -Fb*curvesX[i] + Fa*curvesY[i] + 1.6;
-	newCurvesX[i+lh2] = Fc*curvesX[i] - Fd*curvesY[i];
-	newCurvesY[i+lh2] = Fe*curvesX[i] + Ff*curvesY[i] + 1.6;
-	newCurvesX[i+lh3] = -Fg*curvesX[i] + Fh*curvesY[i];
-	newCurvesY[i+lh3] = Fi*curvesX[i] + Fj*curvesY[i] + 0.44;
+    for (var j=0 ; j < J ; ++j ) {
+        var X1 = 0*X[j];
+	var Y1 = 0.16*Y[j];
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.85*X[j] + 0.04*Y[j];
+	Y1 = -0.04*X[j] + 0.85*Y[j] + M*1.6;
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.20*X[j] - 0.26*Y[j];
+	Y1 = 0.23*X[j] + 0.22*Y[j] + M*1.6;
+    	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = -0.15*X[j] + 0.28*Y[j];
+	Y1 = 0.26*X[j] + 0.24*Y[j] + M*0.44;
+    	newPoints.push(Math.floor(X1),Math.floor(Y1));
     }
-    return new Curves2(newCurvesX, newCurvesY, initNbrVtx);
+    return newPoints;
 }
 
 /* *********************************************************************** */
 
-/* Iterations on the real coordinates */
+/* Iterations on the discretized coordinates of the rescaled IFS */
 function iterateLEAF() {
     var cnvs = document.getElementById("LEAF");
     var width = cnvs.width;
@@ -364,36 +382,38 @@ function iterateLEAF() {
     var choice = Number( document.getElementById("initLEAF").value );
     var fillLeaf = ( document.getElementById("filledLEAF").value === 'true' );
     var colLeaf = document.getElementById("colourLEAF").value;
+    var M = Number( document.getElementById("scaleLEAF").value );
 
-    var curves = initObject4(cnvs, fillLeaf, colLeaf, choice);
+    var points = initPoints(cnvs, M, fillLeaf, colLeaf, choice);
 
     for ( var i = 0 ; i < nbr ; ++i )
-	curves = nextStepLEAF(curves);
+	points = nextStepLEAF(points);
 
     cntxt.clearRect(0,0, width, height);
-    plotCurve2(cnvs, curves, fillLeaf);
+    plotPoints(cnvs, points);
 }
 
-function nextStepLEAF(curves) {
-    var curvesX = curves.curvesX;
-    var curvesY = curves.curvesY;
-    var initNbrVtx = curves.initNbrVtx;
+function nextStepLEAF(points) {
+    var X = points.X;
+    var Y = points.Y;
+    var M = points.M;
+    var J = X.length;
 
-    var lh = curvesX.length;
-    var lh2 = 2*lh;
-    var lh3 = 3*lh;
-    var newCurvesX = new Array(4*lh);
-    var newCurvesY = new Array(4*lh);
+    var newPoints = new Points([],[],M);
 
-    for (var i=0 ; i < lh ; ++i ){
-	newCurvesX[i] = 0.6*curvesX[i] + 0.18;
-	newCurvesY[i] = 0.6*curvesY[i] + 0.36;
-	newCurvesX[i+lh] = 0.6*curvesX[i] + 0.18;
-	newCurvesY[i+lh] = 0.6*curvesY[i] + 0.12;
-	newCurvesX[i+lh2] = 0.4*curvesX[i] + 0.3*curvesY[i] + 0.27;
-	newCurvesY[i+lh2] = -0.3*curvesX[i] + 0.4*curvesY[i] + 0.32;
-	newCurvesX[i+lh3] = 0.4*curvesX[i] - 0.3*curvesY[i] + 0.27;
-	newCurvesY[i+lh3] = 0.3*curvesX[i] + 0.4*curvesY[i] + 0.09;
+    for (var j=0 ; j < J ; ++j ) {
+	var X1 = 0.6*X[j] + M*0.18;
+	var Y1 = 0.6*Y[j] + M*0.36;
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.6*X[j] + M*0.18;
+	Y1 = 0.6*Y[j] + M*0.12;
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.4*X[j] + 0.3*Y[j] + M*0.27;
+	Y1 = -0.3*X[j] + 0.4*Y[j] + M*0.32;
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
+	X1 = 0.4*X[j] - 0.3*Y[j] + M*0.27;
+	Y1 = 0.3*X[j] + 0.4*Y[j] + M*0.09;
+	newPoints.push(Math.floor(X1),Math.floor(Y1));
     }
-    return new Curves2(newCurvesX, newCurvesY, initNbrVtx);
+    return newPoints;
 }
